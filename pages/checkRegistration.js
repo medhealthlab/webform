bbe9bafc8429432d09136e052a95d6a84ecec5f6
umangcodes <umanga.eng@gmail.com>
2023-03-15 16:11:39 -0400

    feat: recaptcha added to the project

--- pages/checkRegistration.js
+++ pages/checkRegistration.js
@@ -1,20 +1,36 @@
-import {useState} from "react"
+import {createRef, useState} from "react"
 import Axios from "axios"
 import {useRouter} from "next/router"
 import Spinner from "../components/spinner/Spinner.js"
+import ReCAPTCHA from "react-google-recaptcha"
 function CheckRegistration() {
     const router = useRouter()
     const [touched, setTouched] = useState()
     const [loading, setLoading] = useState(false)
     const [healthcard, setHealthCard] = useState("Enter Health card")
     const [formError, setFormError] = useState([])
+    const recaptchaRef = createRef()
     const handleChange = (event) => {
       setHealthCard(event.target.value)
+    }
+    const onReCAPTCHAChange = (captchaCode) => {
+      // If the reCAPTCHA code is null or undefined indicating that
+      // the reCAPTCHA was expired then return early
+      if(!captchaCode) {
+        return;
+      }
+      // Else reCAPTCHA was executed successfully so proceed with the 
+      // alert
+      alert(`Hey, ${email}`);
+      // Reset the reCAPTCHA so that it can be executed again if user 
+      // submits another email.
+      recaptchaRef.current.reset();
     }
     const handleSubmit = async () => {
       setLoading(true)
       healthcard.length === 10 ? 
         (
+          recaptchaRef.current.execute(),
           console.log("call action"),
           console.log(process.env.NEXT_PUBLIC_CHECK_REGISTRATION_ENDPOINT),
           await Axios.post(process.env.NEXT_PUBLIC_CHECK_REGISTRATION_ENDPOINT,{ num: healthcard})
@@ -49,6 +65,7 @@ function CheckRegistration() {
     <div className='h-screen flex items-center justify-center'>
       <div className="mb-5 mx-2 py-5 border rounded-xl shadow-md flex flex-col justify-center item-center transition duration-700 ease-in-out absolute z-20 bg-white bg-opacity-25">
         <form onSubmit={(e) => (e.preventDefault(), handleSubmit())} className="px-5">
+          <ReCAPTCHA ref={recaptchaRef} size="invisible" sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY} onChange={onReCAPTCHAChange} />
             <div className='input-field'>
                 <label className='label'>Health card number*</label>
                 {touched ? <p>{touched + formError[0]}</p>:<></>}
