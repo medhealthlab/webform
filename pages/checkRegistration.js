a9a51bb7c0a24e5e6caf25965a768afcd2ae0c16
umangcodes <umanga.eng@gmail.com>
2023-04-04 13:49:18 -0400

    feat: check registration page updated with new features

--- pages/checkRegistration.js
+++ pages/checkRegistration.js
@@ -1,4 +1,4 @@
-import {createRef, useState} from "react"
+import {createRef, useState, useEffect} from "react"
 import Axios from "axios"
 import {useRouter} from "next/router"
 import Spinner from "../components/spinner/Spinner.js"
@@ -8,54 +8,91 @@ function CheckRegistration() {
     const [touched, setTouched] = useState()
     const [loading, setLoading] = useState(false)
     const [healthcard, setHealthCard] = useState("Enter Health card")
+    const [location, setLocation] = useState()
     const [formError, setFormError] = useState([])
     const recaptchaRef = createRef()
+
+    useEffect(()=>{
+      if(!window){
+        setLocation(window.localStorage.getItem("location")).then(() => console.log("location update"))
+        setHealthCard(window.localStorage.getItem("healthcard")).then(() => console.log("HC update"))
+      }
+      setLocation(window.localStorage.getItem("location"));
+    })
+
     const handleChange = (event) => {
       setHealthCard(event.target.value)
     }
     const onReCAPTCHAChange = (captchaCode) => {
-      // If the reCAPTCHA code is null or undefined indicating that
-      // the reCAPTCHA was expired then return early
       if(!captchaCode) {
         return;
       }
-      // Else reCAPTCHA was executed successfully so proceed with the 
-      // alert
-      alert(`Hey, ${email}`);
-      // Reset the reCAPTCHA so that it can be executed again if user 
-      // submits another email.
       recaptchaRef.current.reset();
     }
     const handleSubmit = async () => {
-      setLoading(true)
-      healthcard.length === 10 ? 
-        (
-          recaptchaRef.current.execute(),
-          console.log("call action"),
-          console.log(process.env.NEXT_PUBLIC_CHECK_REGISTRATION_ENDPOINT),
-          await Axios.post(process.env.NEXT_PUBLIC_CHECK_REGISTRATION_ENDPOINT,{ num: healthcard})
-          .then((response) => {
-            if(response.data.resp === "Registered"){
-                setLoading(false),
-                window.localStorage.setItem("healthcard", healthcard), 
-                router.push("/registered")
-              }
-              else{
-                setLoading(false),
-                router.push("/patient/Registration")
-              } 
-              }, 
-              (err) => {
-                setLoading(false),
-                console.log(err)
-              }
-          )
-        )
-        :
-        (
-          setFormError([`length of the health card less than 10`]), 
-          console.log(formError)
-        )
+      // Loading true
+      setLoading(true);
+      
+      (healthcard && location) ? (
+        window.localStorage.setItem("healthcard",healthcard),
+        console.log("health card number: " + healthcard , "from location: " + location),
+        recaptchaRef.current.execute(),
+        // console.log(process.env.NEXT_PUBLIC_CREATE_NEW_VISIT),
+        await Axios.post(process.env.NEXT_PUBLIC_CREATE_NEW_VISIT, {healthcard: healthcard, location: location}).then(async (resp) => {
+          console.log(resp);
+          if(resp.data.msg == "visit created"){
+            window.localStorage.setItem("token", resp.data.token)
+            setLoading(false),
+            console.log("visit created"),
+            router.push("/Registered")
+          }else{
+            setLoading(false),
+            console.log("error occured."),
+            router.push("/patient/Registration")
+          }
+        }, err => {setLoading(false), console.log(err.message)})
+        // await Axios.post(process.env.NEXT_PUBLIC_CREATE_NEW_VISIT,{healthcard: healthcard, location: location}).then(async (response) => {
+        //   console.log(response)
+        // }
+        // )
+      ): (
+        setLoading(false),
+        console.log("something went wrong"),
+        console.log("health card number: " + healthcard , "from location: " + location)
+      )
+
+
+
+
+      // setLoading(true)
+      // console.log(healthcard, location)
+      // // healthcard.length === 12 ? 
+      //   // (
+      //     // window.localStorage.setItem("healthcard", healthcard),
+      //     recaptchaRef.current.execute(),
+      //     await Axios.post(process.env.NEXT_PUBLIC_CREATE_NEW_VISIT,{ healthcard: healthcard, location: location})
+      //     .then(async (response) => {
+      //       console.log(response)
+      //       if(response.data.resp == "visit created"){
+      //           setLoading(false),
+      //           router.push("/registered")
+      //         }
+      //         else{
+      //           setLoading(false),
+      //           router.push("/patient/Registration")
+      //         } 
+      //         }, 
+      //         (err) => {
+      //           setLoading(false),
+      //           console.log(err)
+      //         }
+      //     )
+        // )
+        // :
+        // (
+        //   setFormError([`length of the health card must be equal to 12`]), 
+        //   console.log(formError)
+        // )
     }
     const selectInput = () => {
       touched ? {} : (setHealthCard(""), console.log("reset the input"))
@@ -68,7 +105,7 @@ function CheckRegistration() {
           <ReCAPTCHA ref={recaptchaRef} size="invisible" sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY} onChange={onReCAPTCHAChange} />
             <div className='input-field'>
                 <label className='label'>Health card number*</label>
-                {touched ? <p>{touched + formError[0]}</p>:<></>}
+                {touched ? <p>{formError[0]}</p>:<></>}
                 <input value={healthcard} placeholder="Enter Health card" name="healthcard" onChange={(e) => handleChange(e)} className="classic-input" onSelect={() => selectInput()}/>
                 <br/>
                 {loading ? <Spinner /> : <button type="submit" className="button">Submit</button>}
