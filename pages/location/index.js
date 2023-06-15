f4a66e5ac9d4d8efc3dea9d379ff71aa3167b657
umangcodes <umanga.eng@gmail.com>
2023-06-15 17:47:13 -0400

    feat: value used instead of selected

--- pages/location/index.js
+++ pages/location/index.js
@@ -9,18 +9,18 @@ function Location() {
   useEffect(()=> {
     setLocation(window.localStorage.getItem("location") ? window.localStorage.getItem("location") : "")
   },[])
-
+  
   return (
     <div className="mx-10 flex">
       <div className="border rounded-xl shadow-md px-5 py-10 flex flex-col relative items-center justify-center">
         <div className="pb-5 flex flex-col items-center justify-center">
-          <div className="pb-5">
+          <div className="pb-5 flex flex-col items-center">
             <h1 className="text-xl font-semibold">Which location are you at?</h1>
             <h2 className="text-center text-sm">The number on the top of the QR Code you just scanned.</h2>
           </div>
-          <select onChange={(e) => handleChange(e)} className="focus:border-blue-200 focus:border-2 focus:outline-none border border-blue-100 rounded-xl pl-5" required>
-            <option disabled selected={!location} hidden>select a location</option>
-            <option disabled selected={location} hidden>{location}</option>
+          <select onChange={(e) => handleChange(e)} className="focus:border-blue-200 focus:border-2 focus:outline-none border border-blue-100 rounded-xl px-5 py-2" required>
+            <option disabled value={!location} hidden={location}>select a location</option>
+            <option disabled value={location} hidden={!location}>{location}</option>
             <option disabled className="bg-gray-200 font-semibold">North York</option>
             <option>100</option>
             <option>101</option>
