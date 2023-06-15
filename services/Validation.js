e30f7c542a775f7e1e57d7c5001ee9e6dc24ff8c
umangcodes <umanga.eng@gmail.com>
2023-06-15 17:48:02 -0400

    feat: form validation updated

--- services/Validation.js
+++ services/Validation.js
@@ -6,8 +6,8 @@ export const ValidationSchema = Yup.object().shape({
     middlename : Yup.string().min(2,"To short!").max(50,"To long!"),
     dob : Yup.string().min(2,"To short!").max(50,"To long!").required("Required"),
     healthcard: Yup.string().min(10,"Enter valid healthcard number").max(10,"enter correct health card number").required("Required"),
-    issue : Yup.date().required("Required"),
-    expiry : Yup.date().min(new Date(),"Card expired. Need valid health card.").required("Required"),
+    issueDate : Yup.date().required("Required"),
+    expDate : Yup.date().min(new Date(),"Card expired. Need valid health card.").required("Required"),
     phone: Yup.number("enter a valid number")
     // .test('check for decimal', "cannot contain decimal", val => (console.log(val), !Array.from(val.toString).includes("."))).test('len', "must be 10 digits", val => val.toString().length === 10)
 })
