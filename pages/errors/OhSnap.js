73533fb1467cc24a3e4666d74090db2e94fdbb01
umangcodes <umanga.eng@gmail.com>
2023-06-16 13:36:43 -0400

    chore: on snap page update.

--- pages/errors/OhSnap.js
+++ pages/errors/OhSnap.js
@@ -1,6 +1,6 @@
 function OhSnap() {
   return (
-    <div className="text-xl flex flex-col justify-center items-center h-screen">Oh Snap! something went wrong. Please contact IT support at Med-Health Labs Inc</div>
+    <div className="text-xl flex flex-col justify-center items-center h-screen">Oh Snap! something went wrong. Please contact IT support at Med-Health Labs Inc.</div>
   )
 }
 
