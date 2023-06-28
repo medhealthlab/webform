35449ac07bfd84b0f9f5642a33ea7a1cd399423e
umangcodes <umanga.eng@gmail.com>
2023-06-28 11:59:59 -0400

    feat: form validation modified

--- services/BaseFormValidation.js
+++ services/BaseFormValidation.js
@@ -1,5 +1,5 @@
 import * as Yup from "yup"
 export const BaseFormValidationSchema = Yup.object().shape({
     healthcard: Yup.string().min(10, "Enter your 10 digit health card number").max(10, "Only 10 digits").matches(/^[0-9]+$/, "Must be only digits, do not version code"),
-    file: Yup.mixed().test('fileSize', "file size is too large",value => value.size <= 5000)
+    // file: Yup.mixed().test('fileSize', "file size is too large", value => value.size <= 5000)
 })
