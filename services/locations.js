ac8edc9f985007a6e5a680a1800b2f770e9890e5
umangcodes <umanga.eng@gmail.com>
2023-06-30 11:32:36 -0400

    feat: locations file added to the project

---
+++ services/locations.js
@@ -0,0 +1,154 @@
+export const locations = [
+        {
+            "id": 1,
+            "scc": 100,
+            "address": "1216 Lawernce Ave W North York ON",
+            "city": "North York",
+            "phone" : "",
+            "fax": ""
+        },
+        {
+            "id": 2,
+            "scc": 101,
+            "address": "212 - 2100 Finch Ave W North York ON M3N 2Z9",
+            "city": "North York",
+            "phone" : "4166656142",
+            "fax": "4166652131"
+        },
+        {
+            "id": 3,
+            "scc": 102,
+            "address": "116 - 1315 Finch Ave W North York ON M3J2G6",
+            "city": "North York",
+            "phone": "4166359791",
+            "fax": "4166300173"
+        },
+        {
+            "id": 4,
+            "scc": 201,
+            "address": "200 - 4640 Jane St Toronto ON M3N 2K5",
+            "city": "North York",
+            "phone": "4166618268",
+            "fax": "4166619108"
+        },
+        {
+            "id": 5,
+            "scc": 120,
+            "address": "109 - 2050 Weston Road Toronto ON M3N 2K5",
+            "city": "North York",
+            "phone": "4162351117",
+            "fax": "4162350003"
+        },
+        {
+            "id": 6,
+            "scc": 514,
+            "address": "402 - 3443 Finch Ave E Scarborough ON M1W 2S1",
+            "city": "Scarborough",
+            "phone": "4164976000",
+            "fax": "4164975979"
+        },
+        {
+            "id": 7,
+            "scc": 516,
+            "address": "205 - 3630 Lawrence Ave E Toronto ON M1G 1P6",
+            "city": "Scarborough",
+            "phone": "4162891119",
+            "fax": "4162891911"
+        },
+        {
+            "id": 8,
+            "scc": 701,
+            "address": "4 - 260 Nebo Road Hamilton ON L8W 3K5",
+            "city": "Hamilton",
+            "phone": "2895200603",
+            "fax": "2895200128"
+        },
+        {
+            "id": 9,
+            "scc": 702,
+            "address": "102-631 Queenston Road Hamilton ON L8K 6R5",
+            "city": "Hamilton",
+            "phone": "9055732061",
+            "fax": "9055738042"
+        },
+        {
+            "id": 10,
+            "scc": 450,
+            "address": "102 - 400 Queen Street W Brampton ON L6X 1B3",
+            "city": "Brampton",
+            "phone": "9054507278",
+            "fax": ""
+        },
+        {
+            "id": 11,
+            "scc": 490,
+            "address": "205 - 157 Queen St E Brampton ON L6W 3X4",
+            "city": "Brampton",
+            "phone": "9054510323",
+            "fax": "9054511447"
+        },
+        {
+            "id": 12,
+            "scc": 499,
+            "address": "231 - 2130 North Park Drive Brampton ON L6S 0C9",
+            "city": "Brampton",
+            "phone": "9057908255",
+            "fax": "9054945819"
+        },
+        {
+            "id": 13,
+            "scc": 470,
+            "address": "306 - 3024 Hurontario St Mississauga ON L5B 4M4",
+            "city": "Mississauga",
+            "phone": "9053061580",
+            "fax": "9053067077"
+        },
+        {
+            "id": 14,
+            "scc": 474,
+            "address": "204 - 3420 Hurontario St Mississauga ON L5B 4A9",
+            "city": "Mississauga",
+            "phone": "9058488100",
+            "fax": "9058488101"
+        },
+        {
+            "id": 15,
+            "scc": 495,
+            "address": "5 - 20 Kingsbridge Garden Cir Missi. ON L5B 4A9",
+            "city": "Mississauga",
+            "phone": "9055028886",
+            "fax": "9055028887"
+        },
+        {
+            "id": 16,
+            "scc": 600,
+            "address": "204 - 535 Mulock Dr., New Market ON L3Y 5H2",
+            "city": "New Market",
+            "phone": "9058362227",
+            "fax": "9058362224"
+        },
+        {
+            "id": 17,
+            "scc": 604,
+            "address": "5 - 65 Angeline St. Lindsay ON K9V 5N7",
+            "city": "Lindsay",
+            "phone": "7058780505",
+            "fax": "7053246574"
+        },
+        {
+            "id": 18,
+            "scc": 605,
+            "address": "103 - 15017 Yonge St Aurora ON L4G 1B9",
+            "city": "Aurora",
+            "phone": "9057132226",
+            "fax": "9057262227"
+        },
+        {
+            "id": 19,
+            "scc": 705,
+            "address": "464 Welland Ave St. Catharines ON L2M5V4",
+            "city": "St. Catherines",
+            "phone": "9053785782",
+            "fax": "2893625985"
+        }
+    ]
