0a547ca6d81d45e935e6dc91c9c472ba8800aa3a
umangcodes <umanga.eng@gmail.com>
2023-06-30 11:33:07 -0400

    feat: base validation for the file input changed

--- services/BaseFormValidation.js
+++ services/BaseFormValidation.js
@@ -1,5 +1,18 @@
 import * as Yup from "yup"
 export const BaseFormValidationSchema = Yup.object().shape({
     healthcard: Yup.string().min(10, "Enter your 10 digit health card number").max(10, "Only 10 digits").matches(/^[0-9]+$/, "Must be only digits, do not version code"),
+    photoFile: Yup.mixed().test("fileType", "Unsupported File Format", (value) => {
+        if (value) {
+            return (
+            value.type === "image/jpeg" ||
+            value.type === "image/jpg" ||
+            value.type === "image/png" ||
+            value.type === "application/pdf"
+            );
+        } else {
+            return true;
+        }
+    }
+  )
     // file: Yup.mixed().test('fileSize', "file size is too large", value => value.size <= 5000)
 })
