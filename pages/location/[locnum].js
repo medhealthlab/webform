36b55a22f449be083d5fe04267f8efb2435471d6
umangcodes <umanga.eng@gmail.com>
2023-07-04 11:47:10 -0400

    feat: data context added

--- pages/location/[locnum].js
+++ pages/location/[locnum].js
@@ -1,12 +1,14 @@
 import { useRouter } from "next/router"
-import { useEffect } from "react"
+import { useEffect, useContext } from "react"
+import { Data } from "@/context/dataContext"
 function Location() {
+  const {data, setData} = useContext(Data)
   const ourLocations = [100,101,102,120,201,450,470,474,490,495,499,514,516,600,604,605,701,702,705]  
   const router = useRouter()
     const locnum = parseInt(router.query.locnum)
     const valid = new Set(ourLocations).has(locnum)
     const checkValidity = () => {
-      valid ? (window.localStorage.setItem("location", locnum), router.push("/checkRegistration")) : router.push("/location")
+      valid ? (window.localStorage.setItem("location", locnum), setData((data)=> ({...data, location: locnum})), router.push("/checkReg")) : router.push("/location")
     }
     useEffect(() => {(
                       
