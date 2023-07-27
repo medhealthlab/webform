478800f78d680c261d5f0e7569728e1b7c33c5cf
umangcodes <umanga.eng@gmail.com>
2023-07-27 15:43:17 -0400

    feat: custom input boxes

---
+++ components/inputs/Basic.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+function Basic({title, type, data, error, validationRule, disabled}) {
+    const [selected, setSelected] = useState(false)
+    const [titleData, setTitleData] = [...data]
+    const [titleError, setTitleError] = [...error]
+    useEffect(() => {
+        
+    })
+  return (
+    <fieldset className={`border px-3 rounded-md transition-all ease-in-out duration-300 mt-2 ${selected ? "shadow-md border-blue-500" : "py-1"} ${titleError.length >= 1  ? "border-red-500" : ""}`}>
+          <legend className={`${selected ? "transition ease-in-out duration-300 px-2" : "transition ease-in-out opacity-0 hidden"}`}>{title}</legend>
+          <input placeholder={selected ? "" : title} type={type} value={titleData} onChange={(e) => setTitleData(e.target.value)} onFocus={() => setSelected(true)} onBlur={() => {setSelected(false)}} disabled={disabled ? true : false}className="outline-none text-xl transition-all ease-in-out duration-300"/>
+    </fieldset>
+  )
+}
+
+export default Basic
