dec98d4b1911901e38e30e0b178094482e590fef
umangcodes <umanga.eng@gmail.com>
2023-08-02 12:04:24 -0400

    new data context applied

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '@/styles/globals.css'
 import DataContext from '@/context/dataContext'
-
+import NewDataContext from "@/context/newDataContext"
 export default function App({ Component, pageProps }) {
   return (
       <>
-        <DataContext>
-          <Component {...pageProps} />
-        </DataContext>
+        <NewDataContext>
+          <DataContext>
+            <Component {...pageProps} />
+          </DataContext>
+        </NewDataContext>
       </>
     )
   
