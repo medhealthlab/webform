4c32974ef51ec0b765c16b5db87f7286dffc0d0e
umangcodes <umanga.eng@gmail.com>
2023-08-02 12:03:40 -0400

    registration step 1

--- components/verificationStepsForm/Step1.js
+++ components/verificationStepsForm/Step1.js
@@ -1,21 +1,26 @@
 import Basic from "../inputs/Basic"
-import {useEffect, useState} from "react"
-
+import {useEffect, useState, useContext} from "react"
+import { NewDataContext } from "@/context/newDataContext"
 function Step1() {
-  const [healthcard, setHealthcard] = useState()
-  const [vc, setVc] = useState("")
+  const {state, dispatch} = useContext(NewDataContext)
+  const [vc, setVc] = useState(state.vc ? state.vc : "")
+  const [healthcard, setHealthcard] = useState(state.healthcard ? state.healthcard : "")
+
 
   const [error, setError] = useState([])
+  const handleContextUpdate = (payload) => {
+    dispatch({type: 'UPDATE_DATA', payload: payload})
+  }
+
   useEffect(() => {
-    console.log(healthcard)
-  },[healthcard])
+    handleContextUpdate({healthcard: healthcard, vc: vc})
+  },[healthcard, vc])
+
   return (
     <div className='border rounded-xl h-96 shadow-xl px-5 py-2 '>
-      
       <div className="flex flex-col justify-center items-center gap-5 pt-5">
-        <Basic disabled={true} title={"Healthcard"} type="number" data={[healthcard, setHealthcard]} error={[error, setError]} validationRule={""}/>
+        <Basic disabled={true} title={"Healthcard"} type="string" data={[healthcard, setHealthcard]} error={[error, setError]} validationRule={""}/>
         <Basic title={"Version Code"} type="text" data={[vc, setVc]} error={[error, setError]}/>
-
       </div>
     </div>
   )
