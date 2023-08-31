642ca3fa1086ce67ae5d4dd612d9cedc97ee86e8
umangcodes <umanga.eng@gmail.com>
2023-08-31 11:29:36 -0400

    step 3 : mobile number validation updated.

--- components/form/validation/step3.js
+++ components/form/validation/step3.js
@@ -2,7 +2,7 @@ import * as Yup from "yup"
 
 const Step3Schema = Yup.object().shape({
     email : Yup.string().required("firstname required"),
-    mobile: Yup.number().required().test('len', "digits missing", val => val ? val.length === 10: "")
+    mobile: Yup.string().required().min(10, "provide full number").max(10, "max exceed")
 })
 
 export {Step3Schema}
