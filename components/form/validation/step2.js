053444688999a285452aecb04f7bcf9afb276b79
umangcodes <umanga.eng@gmail.com>
2023-08-31 11:11:28 -0400

    validation for step 2 modified

--- components/form/validation/step2.js
+++ components/form/validation/step2.js
@@ -1,9 +1,9 @@
 import * as Yup from "yup"
 
 const Step2Schema = Yup.object().shape({
-    firstname : Yup.string().min(2,"Valid name must havbe at least 3 char").max(50 , "Name too long").required("firstname required"),
-    lastname: Yup.string().min(2,"Valid name must havbe at least 3 char").max(50 , "Name too long").required("lastname required"),
-    dob: Yup.string().required("dob required"),
+    firstname : Yup.string().min(2,"Valid name must havbe at least 2 char").max(50 , "Name too long").required("firstname required"),
+    lastname: Yup.string().min(2,"Valid name must havbe at least 2 char").max(50 , "Name too long").required("lastname required"),
+    dob: Yup.string().required("dob required").min(8, "complete date of birth").max(8, "complete"),
     sex : Yup.string().required("biological gender required required")
 })
 
