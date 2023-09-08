5fb2d6572490d6767534fd302cab25a72ebc7b86
umangcodes <umanga.eng@gmail.com>
2023-09-08 12:46:24 -0400

    feat: removed other field but toekn number

--- pages/registered.js
+++ pages/registered.js
@@ -17,14 +17,14 @@ function Registered() {
       <h1 className="text-2xl font-semibold">Welcome to <br />Med Health labs Ltd.</h1>
       <div className="border rounded-xl px-5 py-5 flex flex-col mt-5">
         <h2 className="text-xl font-semibold">Token number: <span className="text-2xl animate-pulse">{token}</span></h2>
-        <div className="px-2 py-5">
+        {/* <div className="px-2 py-5"> */}
           {/* <p>SCC number: {location}</p> */}
-          <h2>Your are at</h2>
-          {locations.map((loc) => {return (loc.scc == parseInt(location) ? <p key={loc.id}>{loc.address}</p> : "")})}
-          <p>Your Health Card: {healthcard}</p>
+          {/* <h2>Your are at</h2> */}
+          {/* {locations.map((loc) => {return (loc.scc == parseInt(location) ? <p key={loc.id}>{loc.address}</p> : "")})} */}
+          {/* <p>Your Health Card: {healthcard}</p> */}
           {/* <p>Please be patient and we will serve you in {"time"}.</p> */}
           {/* Current wait times are ...  */}
-        </div>
+        {/* </div> */}
       </div>
           <p className="text-xs absolute bottom-0 mr-12 py-2">If you have any questions, please feel free to speak with any of the staff member.</p>
     </div>
