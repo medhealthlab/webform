0c85689e329306fac97ebb9b9442d72d9616f9e3
umangcodes <umanga.eng@gmail.com>
2023-09-08 11:46:01 -0400

    feat: new logic and redirect to new form added.

--- components/callwindows/ScanHealthcard.js
+++ components/callwindows/ScanHealthcard.js
@@ -1,12 +1,14 @@
-import { useEffect, useState, useContext } from "react"
+import { useState, useContext } from "react"
 import Axios from "axios"
 import { Data } from "@/context/dataContext"
 import { useRouter } from "next/router"
 import Spinner from "../spinner/Spinner"
+import { FormContext } from "@/context/formContext"
 import { NewDataContext } from "@/context/newDataContext"
 function ScanHealthcard({selectedWindow, setSelectedWindow, loading, setLoading}) {
     const {data, setData} = useContext(Data)
-    const {state, dispatch} = useContext(NewDataContext)
+    const {state, dispatch} = useContext(FormContext)
+    // const {state, dispatch} = useContext(NewDataContext)
     const router = useRouter()
     const [parse, setParse] = useState(false)
     const [photoFile, setPhotoFile] = useState()
@@ -29,59 +31,77 @@ function ScanHealthcard({selectedWindow, setSelectedWindow, loading, setLoading}
           fileReader.readAsDataURL(imageFile);
         });
       }
-
+    const handleStateUpdate = (payload) => {
+      console.log("updating state")
+        dispatch({type: "UPDATE_STATE", payload: payload})
+        console.log(state)
+      console.log("state updated")
+    }
     const parseImage = async () => {
     setLoading(true);
     try {
         const imageStr = await convertImageToBase64(photoFile);
         console.log(imageStr)
-        const resp = await Axios.post('https://healthcard-ocr.nn.r.appspot.com/scan', { image: imageStr });
-        console.log(resp)
-        console.log(`name: ${resp.data.firstname}`)
-        console.log(`hc: ${resp.data.heathcard}`)
-        console.log(`dob: ${resp.data.dob}`)
-        console.log(`last: ${resp.data.lastname}`)
-        console.log(`iss: ${resp.data.issueDate}`)
-        console.log(`exp: ${resp.data.expDate}`)
-        dispatch({type: "UPDATE_DATA", payload: {
-                                                  healthcard: resp.data.heathcard.length >= 10 ? resp.data.heathcard.slice(0,10) : "", 
-                                                  vc: resp.data.heathcard.length >=10 ? resp.data.heathcard.slice(10,12) : "" , 
-                                                  firstname: resp.data.firstname ? resp.data.firstname : "NOT DETECTED",
-                                                  middlename: resp.data.middlename ? resp.data.middlename : "NOT DETECTED",
-                                                  lastname: resp.data.lastname ? resp.data.lastname : "NOT DETECTED",
-                                                  // dob: resp.data.dob ? resp.data.dob : ""
-                                                }})
-        if(resp.status == 200){ 
-          setData((data) => ({
-            ...data,
-            healthcard: resp.data.heathcard.slice(0,10),
-            firstname: resp.data.firstname,
-            lastname: resp.data.lastname,
-            dob: resp.data.dob,
-            issueDate: resp.data.issueDate,
-            expDate: resp.data.expDate
-          }))
-          setTimeout(() => {
-          },300)
-        // call another api for validation
-        console.log({healthcard: resp.data.heathcard.slice(0,10), location: data.location})
-        console.log(data.healthcard)
-        await Axios.post(process.env.NEXT_PUBLIC_CREATE_NEW_VISIT, {healthcard: resp.data.heathcard.slice(0,10), location: data.location.toString()}).then(async (resp) => {
-          if(resp.data.msg == "visit created"){
-            window.localStorage.setItem("token", resp.data.token)
-            setData(data => ({...data, token: resp.data.token}))
-            setLoading(false),
-            console.log("visit created"),
-            router.push("/registered")
+        // ocr call
+        await Axios.post('https://healthcard-ocr.nn.r.appspot.com/scan', { image: imageStr }).then(async resp => {
+          console.log(resp)
+          console.log(`name: ${resp.data.firstname}`)
+          console.log(`hc: ${resp.data.heathcard}`)
+          console.log(`dob: ${resp.data.dob}`)
+          console.log(`last: ${resp.data.lastname}`)
+          console.log(`iss: ${resp.data.issueDate}`)
+          console.log(`exp: ${resp.data.expDate}`)
+          const formattedData = {
+              healthcard: resp.data.heathcard.length >= 10 ? resp.data.heathcard.slice(0,10) : "", 
+              vc: resp.data.heathcard.length >=10 ? resp.data.heathcard.slice(10,12) : "" , 
+              firstname: resp.data.firstname ? resp.data.firstname : "NOT DETECTED",
+              middlename: resp.data.middlename ? resp.data.middlename : "NOT DETECTED",
+              lastname: resp.data.lastname ? resp.data.lastname : "NOT DETECTED",
+              // dob: resp.data.dob ? resp.data.dob : ""
+            }
+            console.log(formattedData)
+          if(resp.status == 200){
+            // console.log(resp.data)
+            console.log(state)
+            setData((data) => ({
+              ...data,
+              healthcard: resp.data.heathcard.slice(0,10),
+              vc: resp.data.heathcard.length >=10 ? resp.data.heathcard.slice(10,12) : "" , 
+              firstname: resp.data.firstname,
+              lastname: resp.data.lastname,
+              dob: resp.data.dob,
+              issueDate: resp.data.issueDate,
+              expDate: resp.data.expDate
+            }))
+            console.log(data)
+            // 
+            setTimeout(() => {
+            },400)
+          // firebase for reg and validation
+          console.log({healthcard: resp.data.heathcard.slice(0,10), location: data.location})
+          await Axios.post(process.env.NEXT_PUBLIC_CREATE_NEW_VISIT, {healthcard: resp.data.heathcard.slice(0,10), location: data.location.toString()}).then(async (resp) => {
+            if(resp.data.msg == "visit created"){
+              // generate token mechanism
+              window.localStorage.setItem("token", resp.data.token)
+              setData(data => ({...data, token: resp.data.token}))
+              setLoading(false),
+              console.log("visit created"),
+              router.push("/registered")
+            }else{
+              // send to webforms
+              setLoading(false),
+              handleStateUpdate(data)
+              console.log(data)
+              console.log(state)
+              console.log("error occured."),
+              router.push("/patient/LineUp")
+            }
+          }, err => {setLoading(false), console.log(err.message)})
           }else{
-            setLoading(false),
-            console.log("error occured."),
-            router.push("/patient/RegistrationMultipleStep")
+            router.push("/patient/LineUp")
           }
-        }, err => {setLoading(false), console.log(err.message)})
-        }else{
-          router.push("/patient/RegistrationMultipleStep")
-        }
+        });
+        
     } catch (error) {
         console.error(error);
     }
