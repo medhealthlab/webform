864420e9ab02aaa81de60ec4ea2b547623bac4f1
umangcodes <umanga.eng@gmail.com>
2023-09-08 11:46:59 -0400

    feat: scan function active

--- pages/checkReg.js
+++ pages/checkReg.js
@@ -2,6 +2,7 @@ import { BaseFormValidationSchema } from "@/services/BaseFormValidation"
 import {useFormik, createRef} from "formik"
 import { useEffect, useState, useContext, useRef} from "react"
 import { Data } from "@/context/dataContext"
+import { FormContext } from "@/context/formContext"
 import Axios from "axios"
 import ReCAPTCHA from "react-google-recaptcha"
 import EnterHC from "@/components/callwindows/EnterHC"
@@ -11,7 +12,6 @@ function CheckReg() {
     const [selectedWindow, setSelectedWindow] = useState()
     const [uploadWindow, setUploadWindow] = useState(true)
     let location
-    const {data, setData} = useContext(Data)
     const recaptchaRef = useRef()
     const onReCAPTCHAChange = (captchaCode) => {
         if(!captchaCode) {
@@ -20,41 +20,41 @@ function CheckReg() {
         recaptchaRef.current.reset();
       }
 
-    function convertImageToBase64(imageFile) {
-        return new Promise((resolve, reject) => {
-          const fileReader = new FileReader();
+    // function convertImageToBase64(imageFile) {
+    //     return new Promise((resolve, reject) => {
+    //       const fileReader = new FileReader();
       
-          fileReader.onload = () => {
-            const srcData = fileReader.result;
-            resolve(srcData);
-          };
+    //       fileReader.onload = () => {
+    //         const srcData = fileReader.result;
+    //         resolve(srcData);
+    //       };
       
-          fileReader.onerror = () => {
-            reject(new Error('Error occurred while reading the file.'));
-          };
+    //       fileReader.onerror = () => {
+    //         reject(new Error('Error occurred while reading the file.'));
+    //       };
       
-          fileReader.readAsDataURL(imageFile);
-        });
-      }
+    //       fileReader.readAsDataURL(imageFile);
+    //     });
+    //   }
 
-    const parseImage = async () => {
-    // console.log('parsing the file.');
-    setLoading(true);
-    // console.log('converting');
-    try {
-        const imageStr = await convertImageToBase64(file);
-        const resp = await Axios.post('https://healthcard-ocr.nn.r.appspot.com/scan', { image: imageStr });
-        setData(resp)
-        console.log(resp)
-        handleSubmit()
-    } catch (error) {
-        console.error(error);
-    }
-    };
+    // const parseImage = async () => {
+    // // console.log('parsing the file.');
+    // setLoading(true);
+    // // console.log('converting');
+    // try {
+    //     const imageStr = await convertImageToBase64(file);
+    //     const resp = await Axios.post('https://healthcard-ocr.nn.r.appspot.com/scan', { image: imageStr });
+    //     setData(resp)
+    //     console.log(resp)
+    //     handleSubmit()
+    // } catch (error) {
+    //     console.error(error);
+    // }
+    // };
     useEffect(()=>{
         location = window.localStorage.getItem("location")
         console.log(location)
-        console.log(data)
+        // console.log(data)
     },[uploadWindow])
     return(
         <div onSubmit={(e) => {e.preventDefault(), formik.handleSubmit()} } className="flex justify-center items-center h-screen -mt-48" onMouseDown={() => setSelectedWindow("")}>
