e1d49adeabcbfb92efc8fee14ca9e8ad91654c66
umangcodes <umanga.eng@gmail.com>
2023-09-08 11:46:38 -0400

    feat: minor comestic changes

--- pages/patient/LineUp.js
+++ pages/patient/LineUp.js
@@ -1,26 +1,22 @@
-import { useState, useEffect} from "react"
 import SelectStep from "@/components/form/SelectStep"
-import FormContext from "@/context/formContext"
 
 function RegistrationMultipleStep() {
-
-    
-    useEffect(() => {
-
-    },[])
   return (
-    <FormContext>
-        <div className="flex flex-col m-5 border items-center justify-center">
+
+        <div className="flex flex-col m-5 border items-center justify-center min-h-[75vh]">
           <div className="flex flex-col">
               <div id="reg-form" className=" flex flex-col justify-center items-center m-2" >
-                  <h1 className="text-xl font-semibold">Patient Registration</h1>                
+                  <div className="py-5">
+                    <h1 className="text-xl text-center font-semibold">Line up registration</h1>
+                    <h2 className="text-center -mt-1">Skip the queue by providing your details here.</h2>                
+                  </div>
                   <div id="verification-steps">
                     <SelectStep />
                   </div>
                 </div>
             </div>
+
         </div>
-    </FormContext>
   )
 }
 
