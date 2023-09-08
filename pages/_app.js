f06506ff5ae4908161408d0c3540b7c1a1c7abef
umangcodes <umanga.eng@gmail.com>
2023-09-08 11:47:11 -0400

    feat: new form context added

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 import '@/styles/globals.css'
 import DataContext from '@/context/dataContext'
 import NewDataContext from "@/context/newDataContext"
+import FormContext from "@/context/formContext"
+
 export default function App({ Component, pageProps }) {
   return (
       <>
+      <FormContext>
         <NewDataContext>
           <DataContext>
             <Component {...pageProps} />
           </DataContext>
         </NewDataContext>
+      </FormContext>
       </>
     )
   
