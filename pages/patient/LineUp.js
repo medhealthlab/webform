8846ebbc468a8db0728972de248681d1a522dfae
umangcodes <umanga.eng@gmail.com>
2023-09-14 15:24:29 -0400

    chore: basic text update

--- pages/patient/LineUp.js
+++ pages/patient/LineUp.js
@@ -3,12 +3,12 @@ import SelectStep from "@/components/form/SelectStep"
 function RegistrationMultipleStep() {
   return (
 
-        <div className="flex flex-col m-5 border items-center justify-center min-h-[75vh]">
+        <div className="flex flex-col m-5 items-center justify-center min-h-[75vh]">
           <div className="flex flex-col">
               <div id="reg-form" className=" flex flex-col justify-center items-center m-2" >
                   <div className="py-5">
                     <h1 className="text-xl text-center font-semibold">Line up registration</h1>
-                    <h2 className="text-center -mt-1">Skip the queue by providing your details here.</h2>                
+                    <h2 className="text-center">Skip the queue. Provide your details here.</h2>                
                   </div>
                   <div id="verification-steps">
                     <SelectStep />
